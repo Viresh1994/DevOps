78678f1bdd1f36a2d2711a6cf66c904669e99f5d
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-25 21:28:56 +0530

    REFACTORED: code for while

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ public class Main {
         int[] arr = new int[1000];
         int count=0;
         for(int i=0; i<arr.length; i++){
-            System.out.println(count);
+            System.out.println("using for: "+count);
             count++;
+        }
+        int j=0;
+        while(j<arr.length){
+            System.out.println("using while "+j);
+            j++;
         }
     }
 }
