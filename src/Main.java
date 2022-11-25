029a7d1df04d510a246f4cb7141c7b600c52c0ae
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-25 21:20:11 +0530

    REFACTORED: Added code for array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int[] arr = new int[1000];
+        int count=0;
+        for(int i=0; i<arr.length; i++){
+            System.out.println(count);
+            count++;
+        }
     }
 }
